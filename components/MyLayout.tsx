f90ccc865aea35c09bd8bc0a4621b6331d49ac9b
Kyle McAlpine <kyle@kjm.bio>
2020-04-20 22:50:58 +0100

    Follow next.js learning guide to intro and about page

---
+++ components/MyLayout.tsx
@@ -0,0 +1,17 @@
+import Header from './Header';
+import { FunctionComponent } from 'react';
+
+const layoutStyle = {
+  margin: 20,
+  padding: 20,
+  border: '1px solid #DDD'
+};
+
+const Layout: FunctionComponent = props => (
+  <div style={layoutStyle}>
+    <Header />
+    {props.children}
+  </div>
+);
+
+export default Layout;
---
+++ pages/index.tsx
@@ -0,0 +1,27 @@
+import Layout from '../components/MyLayout';
+import Link from 'next/link';
+import { FunctionComponent } from 'react';
+
+interface PostLinkProps {
+    title: string
+}
+
+const PostLink: FunctionComponent<PostLinkProps> = props => (
+  <li>
+    <Link href={`/post?title=${props.title}`}>
+      <a>{props.title}</a>
+    </Link>
+  </li>
+);
+export default function Blog() {
+  return (
+    <Layout>
+      <h1>My Blog</h1>
+      <ul>
+        <PostLink title="Hello Next.js" />
+        <PostLink title="Learn Next.js is awesome" />
+        <PostLink title="Deploy apps with Zeit" />
+      </ul>
+    </Layout>
+  );
+}
